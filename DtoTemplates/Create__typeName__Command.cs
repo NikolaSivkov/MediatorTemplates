11e4ea182bc0f7c2bac0ede9ad5f7d701705ff20
Nikola Sivkov <erasrz@gmail.com>
2022-01-24 09:48:28 +0200

    move files in right directory

---
+++ DtoTemplates/Create__typeName__Command.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using _namespaceRoot_.Infrastructure.Dtos;
+using _namespaceRoot_.Model;
+
+namespace _namespaceRoot_.Infrastructure.Commands
+{
+    public class Create_typeName_Command : Create_typeName_Dto, IRequest<_typeName_>
+    {
+    }
+}
